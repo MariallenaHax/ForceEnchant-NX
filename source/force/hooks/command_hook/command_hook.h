f5a326986fc6ce11568d58c3d11b0637ee60f020
Alleis Elis <88145194+MariallenaHax@users.noreply.github.com>
2026-09-18 16:57:32 +0900

    Oops

--- source/force/hooks/command_hook/command_hook.h
+++ source/force/hooks/command_hook/command_hook.h
@@ -10,14 +10,14 @@ HOOK_DEFINE_REPLACE(Command) {
 
 //見つけ方
 //commands.generic.num.tooSmallでsearchする。
-//参照元へ飛ぶ。
+//参照元のうち、引数が4つある関数に飛ぶ。
 //その関数がCommand::ValidRange
 
 namespace CommandHook {
 	class ValidRange : public Hook {
 	public:
 		bool Initialize() override {
-			uintptr_t address = SigScan("FF 83 01 D1 FD 7B ? A9 ? ? ? A9 ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? C3 ? ? ? ? ? ? ? ? ? ? ? ? 00 ? ? ? 00"); //26.51
+			uintptr_t address = SigScan("FF ? 03 D1 FD 7B ? A9 FB 43 00 F9 FA 67 ? A9 F8 5F ? A9 F6 57 ? A9 F4 4f ? A9 FD ? ? 91 68 E1"); //26.51
 			if(address)
 			{
 				Command::InstallAtPtr(address);

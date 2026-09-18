11531080eddb3ef4291894bfb6d0f039f282d6e3
Alleis Elis <88145194+MariallenaHax@users.noreply.github.com>
2026-09-18 14:55:15 +0900

    Added how to find signature

--- source/force/hooks/item_enchants/item_enchants.h
+++ source/force/hooks/item_enchants/item_enchants.h
@@ -10,22 +10,28 @@ public:
 
 class EnchantResult {
 public:
-	int enchantResultType;;
+	int enchantResultType;
 	int enchantId;
 	int level;
 };
 
 HOOK_DEFINE_TRAMPOLINE(ItemEnch) {
-    static EnchantResult* Callback(int64_t _this, EnchantResult* result, EnchantMentInstance* en, bool allowNonVanilla) {
-        return Orig(_this, result, en,true);
+    static void Callback(int64_t _this, EnchantResult* result, EnchantMentInstance* en, bool allowNonVanilla) {
+        Orig(_this, result, en,true);
     }
 };
+
+//見つけ方
+//Enchantment of type {} is not compatible with item.でsearchする。
+//参照元へ飛ぶ。
+//その上にあるもっとも浅いelseの下にある引数が4つある関数に飛ぶ。
+//その関数がItemEnchants::canEnchant
 
 namespace ItemEnchantsHook {
 	class canEnchant : public Hook {
 	public:
 		bool Initialize() override {
-			uintptr_t address = SigScan("fd 7b ba a9 fb 0b 00 f9 fa 67 02 a9 f8 5f 03 a9 f6 57 04 a9 f4 4f 05 a9 fd 03 00 91 f4 03 01 aa f3 03 08");
+			uintptr_t address = SigScan("FD 7B BA A9 FB 0B 00 F9 FA 67 02 A9 F8 5F 03 A9 F6 57 04 A9 F4 4F 05 A9 FD 03 00 91 F4 03 01 AA F3 03 08 AA"); //26.51
 			if(address)
 			{
 				ItemEnch::InstallAtPtr(address);
